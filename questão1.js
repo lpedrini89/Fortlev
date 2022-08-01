d249af5a168d6899acb9fe909ba19294969e7052
Leonardo de Aguiar Pedrini <95589497+lpedrini89@users.noreply.github.com>
2022-08-01 12:28:48 -0300

    Add files via upload

---
+++ questão1.js
@@ -0,0 +1,12 @@
+
+let somaImpares=0;
+
+for (i=1; i<=10;i++) {
+    if(i%2!=0){
+        console.log(`Número: ${i}`)
+        somaImpares = i + somaImpares;
+        console.log(`Soma dos Ímpares: + ${somaImpares}`)
+
+    }
+    
+}
